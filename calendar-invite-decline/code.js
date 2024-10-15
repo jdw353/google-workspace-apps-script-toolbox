800cff4bbbec77c7c8152aa10999f06f8c77b806
Justin Wexler <justin.wexler@gmail.com>
2024-10-15 12:48:08 -0400

    Hidden functions

--- calendar-invite-decline/code.js
+++ calendar-invite-decline/code.js
@@ -14,36 +14,36 @@ function main() {
   const date = new Date();
   for (let i = 0; i < NUM_DAYS; i++) {
     Logger.log(date.toDateString());
-    const events = getEventsToDecline({ date: date });
-    if (events.length > 0) deleteEvents({events});
+    const events = getEventsToDecline_({ date: date });
+    if (events.length > 0) deleteEvents_({ events });
     date.setDate(date.getDate() + 1);
   }
 }
 
-function getEventsToDecline({ date }) {
+function getEventsToDecline_({ date }) {
   const sourceEvents = CalendarApp.getCalendarById(SOURCE).getEventsForDay(date);
-  const rejectEvents = sourceEvents.filter((event) => isRejected({ event: event }));
+  const rejectEvents = sourceEvents.filter((event) => isRejected_({ event: event }));
   Logger.log(`Events to delete: ${rejectEvents.length}`)
-  rejectEvents.forEach((event) => logEvent({ event: event }));
+  rejectEvents.forEach((event) => logEvent_({ event: event }));
   return rejectEvents;
 }
 
-function isRejected({ event } = {}) {
+function isRejected_({ event } = {}) {
   const status = event.getMyStatus();
   const title = event.getTitle();
   const startTime = event.getStartTime();
-  const rejection = determineRejection({ status: status, title: title, startTime: startTime });
+  const rejection = determineRejection_({ status: status, title: title, startTime: startTime });
   return rejection;
 }
 
-function determineRejection({ status, title, startTime } = {}) {
+function determineRejection_({ status, title, startTime } = {}) {
   const startFail = startTime.getHours() >= DECLINE_HOURS.AFTER || startTime.getHours() < DECLINE_HOURS.BEFORE;
   const statusFail = status == STATUS;
   const titleFail = BLOCK_LIST.includes(title);
   return statusFail && (startFail || titleFail);
 }
 
-function deleteEvents({events} = {} ) {
+function deleteEvents_({ events } = {}) {
   Logger.log(`Deleting events...`)
   events.forEach((event) => {
     Logger.log(`${event.getTitle()} deleted`);
@@ -51,7 +51,7 @@ function deleteEvents({events} = {} ) {
   });
 }
 
-function logEvent({ event } = {}) {
+function logEvent_({ event } = {}) {
   Logger.log(`Event: ${event.getTitle()}`);
   Logger.log(`Status: ${event.getMyStatus()}`);
   Logger.log(`Start: ${event.getStartTime()}`);
