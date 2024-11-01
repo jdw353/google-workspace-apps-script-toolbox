48b72be19f6b50ade87e04f0026ed6d6ab5e7cbb
Justin Wexler <justin.wexler@gmail.com>
2024-11-01 14:23:15 -0400

    Email allowlist to bypass auto-decline

--- calendar-invite-decline/code.js
+++ calendar-invite-decline/code.js
@@ -5,9 +5,13 @@ const DECLINE_HOURS = {
   AFTER: 20,
   BEFORE: 7
 };
-const BLOCK_LIST = [
-  "CALENDAR_EVENT_NAMES_GO_HERE",
+const ALLOW_USERS = [
+  "LDAP@EMAIL.COM",
+  "LDAP2@EMAIL.COM"
+];
+const REJECT_TITLES = [
   "CALENDAR_EVENT_NAMES_GO_HERE_1",
+  "CALENDAR_EVENT_NAMES_GO_HERE_2",
 ];
 
 function main() {
@@ -23,24 +27,25 @@ function getEventsToDecline_({ startDate, endDate }) {
   const sourceEvents = CalendarApp.getCalendarById(SOURCE).getEvents(startDate, endDate);
   const rejectEvents = sourceEvents.filter((event) => isRejected_({ event: event }));
   Logger.log(`Events to delete: ${rejectEvents.length}`)
-  rejectEvents.forEach((event) => logEvent_({ event: event }));
   return rejectEvents;
 }
 
 function isRejected_({ event } = {}) {
+  const creator = event.getCreators()[0];
   const status = event.getMyStatus();
   const title = event.getTitle();
   const startTime = event.getStartTime();
-  const rejection = determineRejection_({ status: status, title: title, startTime: startTime });
-  Logger.log(`${startTime.toDateString()} | ${title} | ${status} | ${rejection}`);
+  const rejection = determineRejection_({ status: status, title: title, startTime: startTime, creator: creator });
+  Logger.log(`${startTime.toDateString()} | ${title} | ${creator} | ${status} | ${rejection}`);
   return rejection;
 }
 
-function determineRejection_({ status, title, startTime } = {}) {
+function determineRejection_({ status, title, startTime, creator } = {}) {
   const startFail = startTime.getHours() >= DECLINE_HOURS.AFTER || startTime.getHours() < DECLINE_HOURS.BEFORE;
   const statusFail = status == STATUS;
-  const titleFail = BLOCK_LIST.includes(title);
-  return statusFail && (startFail || titleFail);
+  const titleFail = REJECT_TITLES.includes(title);
+  const creatorFail = !ALLOW_USERS.includes(creator);
+  return creatorFail && statusFail && (startFail || titleFail);
 }
 
 function deleteEvents_({ events } = {}) {
@@ -55,4 +60,5 @@ function logEvent_({ event } = {}) {
   Logger.log(`Event: ${event.getTitle()}`);
   Logger.log(`Status: ${event.getMyStatus()}`);
   Logger.log(`Start: ${event.getStartTime()}`);
+  Logger.log(`Creators: ${event.getCreators()}`);
 }
